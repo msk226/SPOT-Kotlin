4ff86e1486a2e32ed88d456adce1b45153be655b
msk226 <kminseok5167@gmail.com>
2026-01-16 20:21:24 +0900

    [Feature] JPA Config 스캔 패키지 지정

--- core/boot/src/main/kotlin/kr/spot/core/config/JpaConfig.kt
+++ core/boot/src/main/kotlin/kr/spot/core/config/JpaConfig.kt
@@ -2,7 +2,16 @@ package kr.spot.core.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories(
+    basePackages = [
+        "kr.spot.member",
+        "kr.spot.notification",
+        "kr.spot.post",
+        "kr.spot.point"
+    ]
+)
 class JpaConfig
--- study/boot/src/main/kotlin/kr/spot/study/config/JpaConfig.kt
+++ study/boot/src/main/kotlin/kr/spot/study/config/JpaConfig.kt
@@ -2,7 +2,9 @@ package kr.spot.study.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = ["kr.spot.study"])
 class JpaConfig
