7e38cdc0fa99b8102ee3bc6a60a0394e2207be06
msk226 <kminseok5167@gmail.com>
2026-01-17 15:11:55 +0900

    [Feature] 회원 생성 시, 출석 스트릭 엔티티도 함께 생성 - 이벤트 기반 구조

---
+++ core/attendance/src/main/java/kr/spot/core/attendance/application/event/AttendanceApplicationEventListener.kt
@@ -0,0 +1,24 @@
+package kr.spot.core.attendance.application.event
+
+import kr.spot.common.id.IdGenerator
+import kr.spot.core.attendance.domain.AttendanceStreak
+import kr.spot.core.attendance.infrastructure.AttendanceStreakRepository
+import org.springframework.stereotype.Component
+import org.springframework.transaction.event.TransactionPhase
+import org.springframework.transaction.event.TransactionalEventListener
+
+@Component
+class AttendanceApplicationEventListener(
+    private val idGenerator: IdGenerator,
+    private val attendanceStreakRepository: AttendanceStreakRepository
+) {
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+    fun registerAttendanceStreak(memberId: Long) {
+        val attendanceStreak =
+            AttendanceStreak.create(
+                id = idGenerator.nextId(),
+                memberId = memberId
+            )
+        attendanceStreakRepository.save(attendanceStreak)
+    }
+}
